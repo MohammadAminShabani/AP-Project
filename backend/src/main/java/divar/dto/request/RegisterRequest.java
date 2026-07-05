00667fa789ee62fd7e16a187da6f2bff0f3ffc34
Mohammadamin_sh <sr.mohammadamin86@gmail.com>
2026-07-05 17:55:05 +0330

    fix: Update RegisterRequest class

--- backend/src/main/java/divar/dto/request/RegisterRequest.java
+++ backend/src/main/java/divar/dto/request/RegisterRequest.java
@@ -11,4 +11,44 @@ public class RegisterRequest {
     private String phoneNumber;
 
     private String email;
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
 }
