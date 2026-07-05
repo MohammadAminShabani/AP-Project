bae8679d8ad96806f59338ad76de5bf6e5480aa5
Mohammad Arshadfard <varshadfard6@gmail.com>
2026-07-05 14:50:34 +0430

    feat: Creat CityRepository interface

---
+++ backend/src/main/java/divar/repository/CityRepository.java
@@ -0,0 +1,12 @@
+package divar.repository;
+
+import divar.entity.City;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface CityRepository extends JpaRepository<City, Long> {
+
+    Optional<City> findByName(String name);
+
+}
