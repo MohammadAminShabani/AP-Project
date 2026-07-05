bbd441c474f668f63ffc4489b4177e361454dfd1
Mohammad Arshadfard <varshadfard6@gmail.com>
2026-07-05 14:53:04 +0430

    feat: Creat FavoriteRepository interface

---
+++ backend/src/main/java/divar/repository/FavoriteRepository.java
@@ -0,0 +1,16 @@
+package divar.repository;
+
+import divar.entity.Advertisement;
+import divar.entity.Favorite;
+import divar.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface FavoriteRepository extends JpaRepository<Favorite, Long> {
+
+    List<Favorite> findByUser(User user);
+
+    boolean existsByUserAndAdvertisement(User user, Advertisement advertisement);
+
+}
