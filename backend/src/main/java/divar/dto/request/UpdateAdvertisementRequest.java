979b2fe589a5bc2656cccdf18adee8fdf61a53ce
Mohammadamin_sh <sr.mohammadamin86@gmail.com>
2026-07-05 17:59:57 +0330

    fix: Update UpdateAdvertisementRequest class

--- backend/src/main/java/divar/dto/request/UpdateAdvertisementRequest.java
+++ backend/src/main/java/divar/dto/request/UpdateAdvertisementRequest.java
@@ -3,12 +3,41 @@ package divar.dto.request;
 public class UpdateAdvertisementRequest {
 
     private String title;
-
     private String description;
+    private double price;
 
-    private Long categoryId;
+    public UpdateAdvertisementRequest() {
+    }
 
-    private Long cityId;
+    public UpdateAdvertisementRequest(String title,
+                                      String description,
+                                      double price) {
+        this.title = title;
+        this.description = description;
+        this.price = price;
+    }
 
-    private Long price;
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
 }
