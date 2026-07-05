d2ee849b273fb779154e5e371630775f33b0e12a
Mohammadamin_sh <sr.mohammadamin86@gmail.com>
2026-07-05 17:59:19 +0330

    fix: Update CreateAdvertisementRequest class

--- backend/src/main/java/divar/dto/request/CreateAdvertisementRequest.java
+++ backend/src/main/java/divar/dto/request/CreateAdvertisementRequest.java
@@ -1,18 +1,64 @@
 package divar.dto.request;
 
-import java.util.List;
-
 public class CreateAdvertisementRequest {
 
     private String title;
-
     private String description;
-
+    private double price;
     private Long categoryId;
-
     private Long cityId;
 
-    private Long price;
+    public CreateAdvertisementRequest() {
+    }
 
-    private List<String> images;
+    public CreateAdvertisementRequest(String title, String description,
+                                      double price,
+                                      Long categoryId,
+                                      Long cityId) {
+        this.title = title;
+        this.description = description;
+        this.price = price;
+        this.categoryId = categoryId;
+        this.cityId = cityId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public Long getCategoryId() {
+        return categoryId;
+    }
+
+    public void setCategoryId(Long categoryId) {
+        this.categoryId = categoryId;
+    }
+
+    public Long getCityId() {
+        return cityId;
+    }
+
+    public void setCityId(Long cityId) {
+        this.cityId = cityId;
+    }
 }
