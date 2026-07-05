18824590d90d66bac0d0ad221adba97a7a3335a2
Mohammadamin_sh <sr.mohammadamin86@gmail.com>
2026-07-05 17:59:34 +0330

    fix: Update RateSellerRequest class

--- backend/src/main/java/divar/dto/request/RateSellerRequest.java
+++ backend/src/main/java/divar/dto/request/RateSellerRequest.java
@@ -2,9 +2,42 @@ package divar.dto.request;
 
 public class RateSellerRequest {
 
-    private Long advertisementId;
-
-    private Integer rate;
-
+    private Long sellerId;
+    private int score;
     private String comment;
+
+    public RateSellerRequest() {
+    }
+
+    public RateSellerRequest(Long sellerId,
+                             int score,
+                             String comment) {
+        this.sellerId = sellerId;
+        this.score = score;
+        this.comment = comment;
+    }
+
+    public Long getSellerId() {
+        return sellerId;
+    }
+
+    public void setSellerId(Long sellerId) {
+        this.sellerId = sellerId;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
 }
