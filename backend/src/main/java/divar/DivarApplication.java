5b16eec32dbe88c05f4e6d61c56578fe93d703ca
Mohammadamin_sh <sr.mohammadamin86@gmail.com>
2026-07-05 12:05:27 +0330

    fix: Update DivarApplication class to test project

--- backend/src/main/java/divar/DivarApplication.java
+++ backend/src/main/java/divar/DivarApplication.java
@@ -1,4 +1,12 @@
 package divar;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class DivarApplication {
+    public static void main(String[] args) {
+        // این خط کل سرور بک‌اند را روشن می‌کند
+        SpringApplication.run(DivarApplication.class , args);
+    }
 }
