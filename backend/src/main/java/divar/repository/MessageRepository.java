dc596e426a6281b986fddc854d5e89de9683f18f
Mohammad Arshadfard <varshadfard6@gmail.com>
2026-07-05 14:52:30 +0430

    feat: Creat MessageRepository interface

---
+++ backend/src/main/java/divar/repository/MessageRepository.java
@@ -0,0 +1,13 @@
+package divar.repository;
+
+import divar.entity.Conversation;
+import divar.entity.Message;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface MessageRepository extends JpaRepository<Message, Long> {
+
+    List<Message> findByConversationOrderBySentAtAsc(Conversation conversation);
+
+}
