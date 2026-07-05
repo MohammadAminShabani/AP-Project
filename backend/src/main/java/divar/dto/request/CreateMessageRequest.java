f617ae40d34116ac93956f01012240ad6243ebb5
Mohammadamin_sh <sr.mohammadamin86@gmail.com>
2026-07-05 17:59:27 +0330

    fix: Update CreateMessageRequest class

--- backend/src/main/java/divar/dto/request/CreateMessageRequest.java
+++ backend/src/main/java/divar/dto/request/CreateMessageRequest.java
@@ -3,6 +3,30 @@ package divar.dto.request;
 public class CreateMessageRequest {
 
     private Long conversationId;
-
     private String content;
+
+    public CreateMessageRequest() {
+    }
+
+    public CreateMessageRequest(Long conversationId,
+                                String content) {
+        this.conversationId = conversationId;
+        this.content = content;
+    }
+
+    public Long getConversationId() {
+        return conversationId;
+    }
+
+    public void setConversationId(Long conversationId) {
+        this.conversationId = conversationId;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
 }
