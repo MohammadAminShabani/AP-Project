27cb47783c9bc855102f65e1264b57bf16526824
Mohammadamin_sh <sr.mohammadamin86@gmail.com>
2026-07-06 15:59:42 +0330

    feat: Create PhoneNumberAlreadyExistsException class.

---
+++ backend/src/main/java/divar/exception/PhoneNumberAlreadyExistsException.java
@@ -0,0 +1,7 @@
+package divar.exception;
+
+public class PhoneNumberAlreadyExistsException extends RuntimeException {
+    public PhoneNumberAlreadyExistsException(String message) {
+        super(message);
+    }
+}
