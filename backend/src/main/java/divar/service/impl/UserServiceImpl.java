6261dd5a5a94cadb70937d799f2951a6580bbf76
Mohammadamin_sh <sr.mohammadamin86@gmail.com>
2026-07-06 16:01:46 +0330

    fix: Creating a registration method.

--- backend/src/main/java/divar/service/impl/UserServiceImpl.java
+++ backend/src/main/java/divar/service/impl/UserServiceImpl.java
@@ -4,6 +4,10 @@ import divar.dto.request.LoginRequest;
 import divar.dto.request.RegisterRequest;
 import divar.dto.response.LoginResponse;
 import divar.dto.response.UserResponse;
+import divar.entity.User;
+import divar.enums.UserRole;
+import divar.enums.UserStatus;
+import divar.exception.*;
 import divar.repository.UserRepository;
 import divar.service.UserService;
 import org.springframework.stereotype.Service;
@@ -21,7 +25,49 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public UserResponse register(RegisterRequest request) {
-        return null;
+
+        if (userRepository.existsByUsername(request.getUsername())) {
+            throw new UsernameAlreadyExistsException("Username already exists.");        }
+
+        if (userRepository.existsByPhoneNumber(request.getPhoneNumber())) {
+            throw new PhoneNumberAlreadyExistsException("Phone number already exists.");
+        }
+        if(request.getPassword() == null || request.getPassword().isEmpty()){
+            throw new PasswordMustNotBeEmptyException("Password must not be empty.");
+        }
+        if (request.getEmail() != null &&
+                !request.getEmail().isBlank() &&
+                userRepository.existsByEmail(request.getEmail())) {
+
+            throw new EmailAlreadyExistsException("Email already exists.");
+        }
+
+        User user = new User();
+
+        user.setFullName(request.getFullName());
+        user.setUsername(request.getUsername());
+        user.setPassword(request.getPassword());
+        user.setPhoneNumber(request.getPhoneNumber());
+        user.setEmail(request.getEmail());
+
+        user.setRole(UserRole.USER);
+        user.setStatus(UserStatus.ACTIVE);
+
+        user.setAverageRating(0);
+        user.setRatingCount(0);
+
+        User savedUser = userRepository.save(user);
+
+        UserResponse response = new UserResponse();
+
+        response.setId(savedUser.getId());
+        response.setFullName(savedUser.getFullName());
+        response.setUsername(savedUser.getUsername());
+        response.setPhoneNumber(savedUser.getPhoneNumber());
+        response.setEmail(savedUser.getEmail());
+        response.setRole(savedUser.getRole());
+
+        return response;
     }
 
     @Override
