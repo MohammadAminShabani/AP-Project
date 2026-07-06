977ac159aab198e082acbf6d7ac41aed87ceb220
Mohammadamin_sh <sr.mohammadamin86@gmail.com>
2026-07-06 16:01:16 +0330

    fix: add Getters And Setter

--- backend/src/main/java/divar/dto/response/UserResponse.java
+++ backend/src/main/java/divar/dto/response/UserResponse.java
@@ -1,5 +1,7 @@
 package divar.dto.response;
 
+import divar.enums.UserRole;
+
 public class UserResponse {
 
     private Long id;
@@ -8,5 +10,69 @@ public class UserResponse {
 
     private String username;
 
-    private Double averageRate;
+    private String phoneNumber;
+
+    private String email;
+
+    private UserRole role;
+
+    public UserResponse(){}
+
+    public UserResponse(Long id , String fullName , String username , String phoneNumber , String email , UserRole role){
+        this.id = id;
+        this.fullName = fullName;
+        this.username = username;
+        this.phoneNumber = phoneNumber;
+        this.email = email;
+        this.role = role;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+    
+    public Long getId() {
+        return id;
+    }
+
+    public UserRole getRole() {
+        return role;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setRole(UserRole role) {
+        this.role = role;
+    }
+
 }
