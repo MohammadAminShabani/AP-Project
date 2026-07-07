4cb1c11ecf09026c79b02f1843606e30241402a3
Mohammad Arshadfard <varshadfard6@gmail.com>
2026-07-07 15:56:04 +0430

    feat: getAllUsers method added

--- backend/src/main/java/divar/service/impl/AdminServiceImpl.java
+++ backend/src/main/java/divar/service/impl/AdminServiceImpl.java
@@ -4,15 +4,30 @@ import divar.dto.response.AdvertisementResponse;
 import divar.dto.response.UserResponse;
 import divar.service.AdminService;
 import org.springframework.stereotype.Service;
-
+import divar.repository.AdvertisementRepository;
+import divar.repository.UserRepository;
 import java.util.List;
 
 @Service
 public class AdminServiceImpl implements AdminService {
 
+    private final UserRepository userRepository;
+    private final AdvertisementRepository advertisementRepository;
+
+    public AdminServiceImpl(UserRepository userRepository,
+                            AdvertisementRepository advertisementRepository) {
+        this.userRepository = userRepository;
+        this.advertisementRepository = advertisementRepository;
+    }
+
     @Override
     public List<UserResponse> getAllUsers() {
-        return List.of();
+        return userRepository.findAll()
+                .stream()
+                .map(user -> new UserResponse(
+                        user.getId(), user.getFullName(), user.getUsername(),
+                        user.getPhoneNumber(), user.getEmail(), user.getRole()))
+                .toList();
     }
 
     @Override
