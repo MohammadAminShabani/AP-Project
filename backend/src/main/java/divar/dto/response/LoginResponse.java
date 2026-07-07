cb13093909c07d8ece814eb66d12d9ec3ef5d7f2
Mohammadamin_sh <sr.mohammadamin86@gmail.com>
2026-07-07 13:00:07 +0330

    fix: Add Getters and Setters to LoginResponse class

--- backend/src/main/java/divar/dto/response/LoginResponse.java
+++ backend/src/main/java/divar/dto/response/LoginResponse.java
@@ -11,4 +11,36 @@ public class LoginResponse {
     private String username;
 
     private UserRole role;
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setRole(UserRole role) {
+        this.role = role;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public UserRole getRole() {
+        return role;
+    }
 }
