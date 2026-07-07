b4bb6791bc9afb19db42bb41112af410f540051d
Mohammadamin_sh <sr.mohammadamin86@gmail.com>
2026-07-07 12:58:53 +0330

    feat: Create InvalidCredentialsException Exception

---
+++ backend/src/main/java/divar/exception/InvalidCredentialsException.java
@@ -0,0 +1,7 @@
+package divar.exception;
+
+public class InvalidCredentialsException extends RuntimeException {
+    public InvalidCredentialsException(String message) {
+        super(message);
+    }
+}
