a544d05de2d80b6600a5eb7926df09bb6138d5c2
Mohammad Arshadfard <varshadfard6@gmail.com>
2026-07-07 16:44:14 +0430

    feat: getAllAdvertisements method added

--- backend/src/main/java/divar/service/impl/AdminServiceImpl.java
+++ backend/src/main/java/divar/service/impl/AdminServiceImpl.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 import divar.repository.AdvertisementRepository;
 import divar.repository.UserRepository;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class AdminServiceImpl implements AdminService {
@@ -32,7 +33,24 @@ public class AdminServiceImpl implements AdminService {
 
     @Override
     public List<AdvertisementResponse> getAllAdvertisements() {
-        return List.of();
+        return advertisementRepository.findAll()
+                .stream()
+                .map(ad -> new AdvertisementResponse(
+                        ad.getId(),
+                        ad.getTitle(),
+                        ad.getDescription(),
+                        ad.getPrice(),
+                        ad.getCity().getName(),
+                        ad.getCategory().getName(),
+                        ad.getStatus(),
+                        ad.getOwner().getFullName(),
+                        ad.getOwner().getAverageRating(),
+                        ad.getImages()
+                                .stream()
+                                .map(image -> image.getImageUrl())
+                                .collect(Collectors.toList())
+                ))
+                .toList();
     }
 
     @Override
