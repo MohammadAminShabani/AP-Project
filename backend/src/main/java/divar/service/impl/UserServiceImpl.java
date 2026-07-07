40967667fc52f425cb0fc0aaabff7c69b39c1b4f
Mohammadamin_sh <sr.mohammadamin86@gmail.com>
2026-07-07 13:02:11 +0330

    fix: Add LoginResponse method to UserServiceImpl class

--- backend/src/main/java/divar/service/impl/UserServiceImpl.java
+++ backend/src/main/java/divar/service/impl/UserServiceImpl.java
@@ -9,6 +9,7 @@ import divar.enums.UserRole;
 import divar.enums.UserStatus;
 import divar.exception.*;
 import divar.repository.UserRepository;
+import divar.security.JwtService;
 import divar.service.UserService;
 import org.springframework.stereotype.Service;
 
@@ -18,9 +19,11 @@ import java.util.List;
 public class UserServiceImpl implements UserService {
 
     private final UserRepository userRepository;
+    private final JwtService jwtService;
 
-    public UserServiceImpl(UserRepository userRepository) {
+    public UserServiceImpl(UserRepository userRepository , JwtService jwtService) {
         this.userRepository = userRepository;
+        this.jwtService = jwtService;
     }
 
     @Override
@@ -72,9 +75,29 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public LoginResponse login(LoginRequest request) {
-        return null;
-    }
 
+        User user = userRepository.findByUsername(request.getUsername())
+                .orElseThrow(() ->
+                        new InvalidCredentialsException("Invalid username or password."));
+
+        if (!user.getPassword().equals(request.getPassword())) {
+            throw new InvalidCredentialsException("Invalid username or password.");
+        }
+
+        if (user.getStatus() != UserStatus.ACTIVE) {
+            throw new UserNotActiveException("User account is not active.");
+        }
+
+        LoginResponse response = new LoginResponse();
+
+        String token = jwtService.generateToken(user);
+        response.setToken(token);
+        response.setUserId(user.getId());
+        response.setUsername(user.getUsername());
+        response.setRole(user.getRole());
+
+        return response;
+    }
     @Override
     public UserResponse findById(Long id) {
         return null;
