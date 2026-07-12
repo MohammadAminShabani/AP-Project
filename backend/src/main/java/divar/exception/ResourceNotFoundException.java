ca7c152cb795b6e5bcd18bffd147cf23d849dd62
Mohammadamin_sh <sr.mohammadamin86@gmail.com>
2026-07-12 17:08:16 +0330

    feat: Create ResourceNotFoundException exception

---
+++ backend/src/main/java/divar/exception/ResourceNotFoundException.java
@@ -0,0 +1,7 @@
+package divar.exception;
+
+public class ResourceNotFoundException extends RuntimeException {
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+}
