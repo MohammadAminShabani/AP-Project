59663af9a157a1239d53d847d1b1b3491ea90e2f
Mohammadamin_sh <sr.mohammadamin86@gmail.com>
2026-07-12 17:03:27 +0330

    feat: Create CityController class

---
+++ backend/src/main/java/divar/controller/CityController.java
@@ -0,0 +1,27 @@
+package divar.controller;
+
+import divar.dto.response.CityResponse;
+import divar.service.CityService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/cities")
+public class CityController {
+
+    private final CityService cityService;
+
+    public CityController(CityService cityService) {
+        this.cityService = cityService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<CityResponse>> getAll() {
+        return ResponseEntity.ok(cityService.getAll());
+    }
+
+}
