65a01d46ad39052c15e3ce2778ace9379f47c86d
Mohammadamin_sh <sr.mohammadamin86@gmail.com>
2026-07-12 17:02:49 +0330

    feat: Create CategoryController class

---
+++ backend/src/main/java/divar/controller/CategoryController.java
@@ -0,0 +1,25 @@
+package divar.controller;
+
+import divar.dto.response.CategoryResponse;
+import divar.service.CategoryService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/categories")
+public class CategoryController {
+
+    private final CategoryService categoryService;
+
+    public CategoryController(CategoryService categoryService) {
+        this.categoryService = categoryService;
+    }
+
+    @GetMapping
+    public List<CategoryResponse> getAllCategories() {
+        return categoryService.getAllCategories();
+    }
+}
