77ecaebc4d72b505725467d9ab58bd7c62f4e11b
Mohammadamin_sh <sr.mohammadamin86@gmail.com>
2026-07-20 12:53:17 +0330

    feat: Create HomeController class

--- frontend/src/main/java/divar/controller/HomeController.java
+++ frontend/src/main/java/divar/controller/HomeController.java
@@ -1,4 +1,28 @@
 package divar.controller;
 
+import divar.config.SceneManager;
+import divar.network.TokenManager;
+import divar.util.Constants;
+import divar.util.SessionManager;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
 public class HomeController {
+
+    @FXML
+    private Label welcomeLabel;
+
+    @FXML
+    public void initialize() {
+        welcomeLabel.setText("خوش آمدید، " + SessionManager.getUsername());
+    }
+
+    @FXML
+    public void logout() {
+
+        TokenManager.clearToken();
+        SessionManager.logout();
+
+        SceneManager.loadScene(Constants.LOGIN, "ورود");
+    }
 }
