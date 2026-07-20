3ba93a03e65f56169690d26e6421a57b030969dd
Mohammadamin_sh <sr.mohammadamin86@gmail.com>
2026-07-20 12:54:00 +0330

    feat: Update module-info.java

--- frontend/src/main/java/module-info.java
+++ frontend/src/main/java/module-info.java
@@ -1,8 +1,16 @@
 module org.example.frontend {
+
     requires javafx.controls;
     requires javafx.fxml;
     requires java.net.http;
+    requires com.fasterxml.jackson.databind;
 
-    opens divar to javafx.fxml;
     exports divar;
+
+    exports divar.controller;
+
+    opens divar.controller to javafx.fxml;
+
+    opens divar.dto.request to com.fasterxml.jackson.databind;
+    opens divar.dto.response to com.fasterxml.jackson.databind;
 }
