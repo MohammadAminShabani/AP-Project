eb2a69c20a5e240acd8fb498dde04ed4e688e2cd
Mohammadamin_sh <sr.mohammadamin86@gmail.com>
2026-07-21 11:51:05 +0330

    fix: Update HttpClientUtil.java

--- frontend/src/main/java/divar/util/HttpClientUtil.java
+++ frontend/src/main/java/divar/util/HttpClientUtil.java
@@ -5,6 +5,7 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import divar.session.SessionManager;
 
 public class HttpClientUtil {
 
