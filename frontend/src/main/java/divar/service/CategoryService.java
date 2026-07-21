7666660d4372963c880c3f051120a35793dbdf68
Mohammadamin_sh <sr.mohammadamin86@gmail.com>
2026-07-21 11:45:36 +0330

    feat: create CategoryService.java

--- frontend/src/main/java/divar/service/CategoryService.java
+++ frontend/src/main/java/divar/service/CategoryService.java
@@ -1,4 +1,29 @@
 package divar.service;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import divar.dto.response.CategoryResponse;
+import divar.network.ApiClient;
+import divar.network.ApiException;
+
+import java.io.IOException;
+import java.util.List;
+
 public class CategoryService {
+
+    private static final ObjectMapper mapper =
+            new ObjectMapper();
+
+    public List<CategoryResponse> getAll()
+            throws IOException, InterruptedException, ApiException {
+
+        String json =
+                ApiClient.get("/categories");
+
+        return mapper.readValue(
+                json,
+                new TypeReference<List<CategoryResponse>>() {}
+        );
+    }
+
 }
